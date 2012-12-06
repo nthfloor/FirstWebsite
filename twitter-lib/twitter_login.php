aa6ba6a3775c05a4348ad9f0ff7a6ed38fa6c6ba
Nathan <nathan.floor@gmail.com>
2012-12-06 12:11:18 +0200

    latest twitter changes

--- twitter-lib/twitter_login.php
+++ twitter-lib/twitter_login.php
@@ -1,14 +1,14 @@
 <?php
-require('../twitteroauth-master/twitteroauth/twitteroauth.php');
-include '../twitter-lib/secret.php';
+require('twitteroauth/twitteroauth.php');
+require_once('config.php');
 
 session_start();
 
 //twitter Oauth instance
-$twitteroauth = new TwitterOAuth($consumer_key, $consumer_secret);
+$twitteroauth = new TwitterOAuth('5sNShhwrP4VxJwpElTyfA', 'j6eajclJ07iHiB07pBrFULTOPWWcT4l17zaC2fmrQ');
 
 //request auth tokens, parameter is URL to be redirected to
-$request_token=$twitteroauth->getRequestToken('http://10.0.0.27/TwitterTest/index.php');
+$request_token=$twitteroauth->getRequestToken('http://105.237.0.43/index.php');
 //saving them into session
 $_SESSION['oauth_token']=$request_token['oauth_token'];
 $_SESSION['oauth_token_secret']=$request_token['oauth_token_secret'];
